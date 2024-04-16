63bcdc2e72a2ba3015a8c83469a011867443f186
Verneri <verneri.lopperi@gmail.com>
2024-04-16 14:36:20 +0300

    event handling

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Item from './components/Item/Item';
-
+import MyText from './components/MyText/MyText';
 const App = () => {
   return (
     <SafeAreaView>
-      <Item name={'Table'} price={20} />
-      <Item name={'Chair'} price={100} />
-      <Item name="desk" price={200} />
+      <MyText />
     </SafeAreaView>
   );
 };
--- components/MyText/MyText.js
+++ components/MyText/MyText.js
@@ -3,8 +3,14 @@ import {Text} from 'react-native';
 import style from './style';
 
 const MyText = () => {
+  const handleTextClick = () => {
+    alert('The text has been clicked!');
+  };
+
   return (
-    <Text style={[style.text, style.text1, {color: 'green'}]}>
+    <Text
+      style={[style.text, style.text1, {color: 'green'}]}
+      onPress={() => handleTextClick()}>
       Hello, react native world
     </Text>
   );
