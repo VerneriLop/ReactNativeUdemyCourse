c9a6ed202413257b5aa98211d62638825742a77e
Verneri <verneri.lopperi@gmail.com>
2024-04-16 14:48:02 +0300

    useState training

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import MyText from './components/MyText/MyText';
+import React, {useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+//import MyText from './components/MyText/MyText';
+
 const App = () => {
+  const [text, setText] = useState('Hello world!');
   return (
     <SafeAreaView>
-      <MyText />
+      {/*<MyText />*/}
+      <Text onPress={() => setText('Hello world, text has changed!')}>
+        {text}
+      </Text>
     </SafeAreaView>
   );
 };
