d24e2e38bb164312359ef50d706911c5d50b78a9
Verneri <verneri.lopperi@gmail.com>
2024-04-16 16:12:59 +0300

    useRef training

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {SafeAreaView, Text, ScrollView, Button} from 'react-native';
 //import MyText from './components/MyText/MyText';
 
 const App = () => {
-  const [text, setText] = useState('Hello world!');
+  /*const [text, setText] = useState(0);
+
+  useEffect(() => {
+    console.log('The text has changed!');
+  }, [text]);
+
+  useEffect(() => {
+    console.log('component has rendered!');
+  }, []);*/
+
+  let array = Array(1000).fill(0);
+  const scrollViewRef = useRef(null);
+
+  const handleClick = () => {
+    scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
+  };
+
   return (
     <SafeAreaView>
-      {/*<MyText />*/}
-      <Text onPress={() => setText('Hello world, text has changed!')}>
-        {text}
-      </Text>
+      {/*<MyText />
+      <Text onPress={() => setText(text + 1)}>{text}</Text>*/}
+      <ScrollView ref={scrollViewRef}>
+        {array.map((value, index) => (
+          <Text key={index}>Hello world! {index}</Text>
+        ))}
+      </ScrollView>
+      <Button onPress={handleClick} title="Scroll to top" />
     </SafeAreaView>
   );
 };
