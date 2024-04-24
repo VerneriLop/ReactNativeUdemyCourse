54493eb29067b88af797cb8c42c13d9782d85505
Verneri <verneri.lopperi@gmail.com>
2024-04-24 14:19:12 +0300

    Handling images

--- App.tsx
+++ App.tsx
@@ -1,4 +1,37 @@
-/*import React, {useState, useEffect, useRef} from 'react';
+import React, {useState} from 'react';
+import {SafeAreaView, Image} from 'react-native';
+
+const App = () => {
+  const [imageSource, setImageSource] = useState({uri: 'http://example.com'});
+
+  return (
+    <SafeAreaView>
+      <Image
+        source={imageSource}
+        style={{width: '100%', height: '100%', backgroundColor: 'red'}}
+        resizeMode={'center'} //contain, stretch, repeat
+        onError={() => {
+          console.log('Error has been detected while loading an image');
+          setImageSource(require('./assets/images/cake.png'));
+        }}
+      />
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80',
+        }}
+        style={{width: 100, height: 100}}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+/*
+
+FUNCTIONAL COMPONENT
+
+import React, {useState, useEffect, useRef} from 'react';
 import {SafeAreaView, Text, ScrollView, Button, View} from 'react-native';
 import MyText from './components/MyText/MyText';
 import {ThemeContext} from './contexts/ThemeContext';
@@ -54,6 +87,12 @@ const App = () => {
 
 export default App;
 */
+
+//----------------------------
+
+/*
+
+CLASS BASED COMPONENT
 
 import React, {Component} from 'react';
 import {SafeAreaView, Text} from 'react-native';
@@ -112,3 +151,4 @@ class App extends Component {
 }
 
 export default App;
+*/
