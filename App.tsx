ead90abf917f37c97331cdf659776c7f62ad9138
Verneri <verneri.lopperi@gmail.com>
2024-04-24 10:41:14 +0300

    Class based components and state managing

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
+/*import React, {useState, useEffect, useRef} from 'react';
 import {SafeAreaView, Text, ScrollView, Button, View} from 'react-native';
-//import MyText from './components/MyText/MyText';
+import MyText from './components/MyText/MyText';
 import {ThemeContext} from './contexts/ThemeContext';
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import useToggle from './customHooks/useToggle';
@@ -8,7 +8,7 @@ import useToggle from './customHooks/useToggle';
 const App = () => {
   const [isOn, toggleIsOn] = useToggle(false);
   const [isDarkMode, setIsDarkMode] = useState(false);
-  /*const [text, setText] = useState(0);
+  const [text, setText] = useState(0);
 
   useEffect(() => {
     console.log('The text has changed!');
@@ -28,29 +28,54 @@ const App = () => {
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
-  */
 
   return (
     <ThemeContext.Provider value={isDarkMode}>
       <SafeAreaView>
-        {/*<MyText />
-      <Text onPress={() => setText(text + 1)}>{text}</Text>
-      <ScrollView ref={scrollViewRef}>
-        {array.map((value, index) => (
-          <Text key={index}>Hello world! {index}</Text>
-        ))}
-      </ScrollView>
-      <Button onPress={handleClick} title="Scroll to top" />
+        <MyText />
+        <Text onPress={() => setText(text + 1)}>{text}</Text>
+        <ScrollView ref={scrollViewRef}>
+          {array.map((value, index) => (
+            <Text key={index}>Hello world! {index}</Text>
+          ))}
+        </ScrollView>
+        <Button onPress={handleClick} title="Scroll to top" />
         <View style={{backgroundColor: isDarkMode ? '#222222' : '#ffffff'}}>
           <Text>Hello world!</Text>
           <Button title="Switch Mode" onPress={toggleTheme} />
         </View>
-        <HomeScreen />*/}
+        <HomeScreen />
         <Text>{isOn ? 'ON' : 'OFF'}</Text>
         <Button title="Toggle" onPress={toggleIsOn} />
       </SafeAreaView>
     </ThemeContext.Provider>
   );
 };
+
+export default App;
+*/
+
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Text
+          onPress={() => {
+            this.setState({name: 'Verneri'});
+          }}>
+          Hello, {this.state?.name}
+        </Text>
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
